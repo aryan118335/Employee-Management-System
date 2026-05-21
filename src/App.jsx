42796c7a5350587562329419730e40f43467804a
Aryan <deshmukharyan901@gmail.com>
2026-05-21 10:57:30 +0530

    Project Initialised, created folder structure and created login page.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <EmployeeDashboard/>
+
+    </>
+  )
+}
+
+export default App
+
+
+// function App() {
+//   return (
+    // <>
+    //   <Login />
+    // </>
+//   )
+// }
+
+// export default App
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
+
+import { defineConfig } from 'vite'
+// import react from '@vitejs/react-swc' // or @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
+
+
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Add this line
+  ],
+})
