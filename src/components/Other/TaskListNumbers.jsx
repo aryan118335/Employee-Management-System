714423038a7c2a5edb3d493fd7b55812de8b805d
Aryan <deshmukharyan901@gmail.com>
2026-05-25 19:52:32 +0530

    Task List

---
+++ src/components/Other/TaskListNumbers.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function TaskListNumbers() {
+  return (
+    <div className='flex mt-10  justify-between gap-5  screen'>
+      <div className='bg-pink-100 py-6 px-9 w-[45%] rounded-xl'>
+        <h2 className='text-black text-3xl font-semibold'>0</h2>
+        <h3 className='text-black text-xl font-medium'>New Task</h3>
+      </div>
+      <div className='bg-blue-100 py-6 px-9 w-[45%] rounded-xl'>
+        <h2 className='text-black text-3xl font-semibold'>0</h2>
+        <h3 className='text-black text-xl font-medium'>New Task</h3>
+      </div>
+      <div className='bg-red-300 py-6 px-9 w-[45%] rounded-xl'>
+        <h2 className='text-black text-3xl font-semibold'>0</h2>
+        <h3 className='text-black text-xl font-medium'>New Task</h3>
+      </div>
+      <div className='bg-emerald-100 py-6 px-9 w-[45%] rounded-xl'>
+        <h2 className='text-black text-3xl font-semibold'>0</h2>
+        <h3 className='text-black text-xl font-medium'>New Task</h3>
+      </div>
+      
+    </div>
+  )
+}
+
+export default TaskListNumbers
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className=' overflow-x-auto mt-15 h-[45%] w-full.  py-5 flex items-start gap-7 flex-nowrap'>
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-100 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-200 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-500 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+        
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-300 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-400 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+        
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-400 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-300 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+
+
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-500 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-200 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+
+
+        <div className='flex-shrink-0 p-5 h-full w-[24%] bg-emerald-600 rounded-xl'>
+            
+            <div className='flex justify-between items-center'>
+                <h3 className='text-black text-xl bg-red-100 px-3 py-1 rounded'>High</h3>
+                <h3 className='text-black '>29 February 2026</h3>
+            </div>
+
+            <h2 className='text-3xl text-black py-6'>Start creating content</h2>
+            <h3 className='text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga dolor, excepturi quia consequuntur consequatur?</h3>
+        </div>
+
+
+        
+    </div>
+  )
+}
+
+export default TaskList
